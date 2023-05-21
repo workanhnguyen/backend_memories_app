e1cdc90c33b5a5d8473d73ac5e00f41eee8027df
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-05-21 15:07:24 +0700

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.CONNECTION_URL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
